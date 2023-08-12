c6821c091a0b6659bfd8cd8d8eb90afd052e4ead
Luis Ramos <120321858+LuisRamosOfficial@users.noreply.github.com>
2023-08-12 22:24:54 +0000

    Search Function Done

--- main.py
+++ main.py
@@ -1,25 +1,10 @@
-import requests, json5, json
-from bs4 import BeautifulSoup
+from modules import get_JsonSearch
+
+url = 'https://www.youtube.com/results?search_query=Ricardo+Salvatore'
+dicti = get_JsonSearch(url)
 
 
-html = requests.get('https://www.youtube.com/results?search_query=the+world+is+yours').text
-soup = BeautifulSoup(html, 'lxml')
-varDataIndex = str(soup).index('var ytInitialData')
-# varFinalIndex = str(soup).index(";</script><script nonce=\"nh2TZ6y_j22p7BqlB_fKaA\"")
-ytInitialData = str(soup)[varDataIndex:]
 
-last = 0
-index = 0
-for n in range(6):
-    if n == 0:
-        index = ytInitialData.rindex('</script>')
-        last = ytInitialData[:index]
-    else:
-        index = last.rindex('</script>')
-        last = last[:index]
-
-py_obj = json5.loads(str(last[19:-1]))
-json_object = json.dumps(py_obj, indent = 4)
 
 with open('okay.json', "w", encoding="utf-8") as f:
-    f.write(json_object)
+    f.write(str(dicti))
---
+++ modules.py
@@ -0,0 +1,60 @@
+import requests, json5, json
+from bs4 import BeautifulSoup
+
+
+def get_JsonSearch(url):
+    html = requests.get(url).text
+    soup = BeautifulSoup(html, "lxml")
+    InitialIndex = str(soup).index("var ytInitialData")
+    StartCutText = str(soup)[InitialIndex:]
+
+    last = 0
+    index = 0
+    for n in range(6):
+        if n == 0:
+            index = StartCutText.rindex("</script>")
+            last = StartCutText[:index]
+        else:
+            index = last.rindex("</script>")
+            last = last[:index]
+
+    py_list = json5.loads(str(last[19:-1]))["contents"][
+        "twoColumnSearchResultsRenderer"
+    ]["primaryContents"]["sectionListRenderer"]["contents"][0]["itemSectionRenderer"][
+        "contents"
+    ]
+
+    video_list = []
+
+    for n in py_list:
+        if list(n.keys())[0] == "videoRenderer":
+            
+            video = n['videoRenderer']
+            thumb = video["thumbnail"]["thumbnails"][0]["url"]
+            title = video["title"]["runs"][0]["text"]
+            channel = video["longBylineText"]["runs"][0]["text"]
+            date = video["publishedTimeText"]["simpleText"]
+            time_duration = video["lengthText"]["accessibility"]["accessibilityData"][
+                "label"
+            ]
+            time_short = video["lengthText"]["simpleText"]
+            views = video["viewCountText"]["simpleText"]
+            url = (
+                "https://youtube.com"
+                + video["navigationEndpoint"]["commandMetadata"]["webCommandMetadata"][
+                    "url"
+                ]
+            )
+
+            video_list.append({"title": title, 
+                               "thumbnail": thumb,
+                               "channel": channel,
+                               "date": date,
+                               "length": time_duration,
+                               "length_short": time_short,
+                               "views": views,
+                               "url": url
+                               })
+
+    video_json = json.dumps(video_list, indent=4)
+    return video_json
